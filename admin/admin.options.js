a47e7958ddc479d89ff92bb35c24503be54877a5
SANNGESH P L <58561823+sanngesh23@users.noreply.github.com>
2021-01-10 13:44:48 +0530

    Update admin.options.js

--- admin/admin.options.js
+++ admin/admin.options.js
@@ -1,7 +1,7 @@
 const { default: AdminBro } = require('admin-bro');
 const AdminBroMongoose = require('@admin-bro/mongoose');
 AdminBro.registerAdapter(AdminBroMongoose);
-const User =require('../models/User');
+const User =require('../models/model');
 const options = {
   resources: [User],
  };
